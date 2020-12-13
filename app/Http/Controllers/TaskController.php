1863b306c5e894a9bdae7b08160c6afe51e5d503
hu dong lin <benabcd5779@gmail.com>
2020-12-13 22:40:14 +0800

    在TaskController中新增回傳index的方法及增加任務的store方法

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,4 +12,16 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
+
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:255',
+        ]);
+    }
 }
